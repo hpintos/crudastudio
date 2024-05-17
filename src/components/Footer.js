fa74cbe52f46cf44a7f8a844a60d19813b88bf39
Hernan Pintos <hernan.pintos@adevinta.com>
2024-05-17 18:17:44 +0200

    Building the main page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,36 @@
 import Image from "next/image";
 import { IconInstagram } from "./Icons/IconInstagram";
 import Link from "next/link";
+import { IconWhatsapp } from "./Icons/IconWhatsapp";
 
 const Footer = () => {
   const crudaLogoSrc = "/images/isologo.png";
 
   return (
-    <footer className="fix bottom-0 grid grid-cols-3 justify-center px-8 py-8">
+    <footer className="fix bottom-0 grid grid-cols-1 sm:grid-cols-3 justify-center px-8 py-8">
       <div className="flex flex-col gap-5">
-        <h2>CrudaStudio</h2>
         <Link href="https://www.instagram.com/crudastudio.es/" target="_blank">
           <IconInstagram />
         </Link>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center p-12 sm:p-0">
         <Image
+          className="w-12 h-12"
           src={crudaLogoSrc}
           alt="Logo de CrudaStudio"
-          width={50}
-          height={50}
+          width="50"
+          height="50"
         />
       </div>
-      <div className="flex justify-center">
-        <h2>Información de contacto</h2>
+      <div className="flex flex-col gap-5">
+        <h2 className="">Información de contacto</h2>
+        <Link href="mailto:crudastudio.es@gmail.com" target="_blank">
+          <span>crudastudio.es@gmail.com</span>
+        </Link>
+
+        <Link href="https://wa.me/+34695848765" target="_blank">
+          <span className="underline">Contacto por Whatsapp</span>
+        </Link>
       </div>
     </footer>
   );
